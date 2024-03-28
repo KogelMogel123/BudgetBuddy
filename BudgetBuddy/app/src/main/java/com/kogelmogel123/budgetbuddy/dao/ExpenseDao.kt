5fe925f0bd86dacf5e6a8aac6b1ebdb368e5177b
Kamil Lewandowski <klewandowski16@gmail.com>
2024-03-28 23:22:17 +0100

    Add ExpenseDao interface

---
+++ BudgetBuddy/app/src/main/java/com/kogelmogel123/budgetbuddy/dao/ExpenseDao.kt
@@ -0,0 +1,11 @@
+@Dao
+interface ExpenseDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(expense: Expense)
+
+    @Update
+    suspend fun update(expense: Expense)
+
+    @Delete
+    suspend fun delete(expense: Expense)
+}
