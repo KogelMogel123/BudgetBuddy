6cd368f82e23f5a9e9720ed2dd9168383ad9c909
Kamil Lewandowski <klewandowski16@gmail.com>
2024-03-29 23:11:24 +0100

    Add import to ExpenseDao

--- BudgetBuddy/app/src/main/java/com/kogelmogel123/budgetbuddy/dao/ExpenseDao.kt
+++ BudgetBuddy/app/src/main/java/com/kogelmogel123/budgetbuddy/dao/ExpenseDao.kt
@@ -1,3 +1,10 @@
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Update
+import com.kogelmogel123.budgetbuddy.model.Expense
+
 @Dao
 interface ExpenseDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
