0885afd5a80114e3f0174cd8c9cbd97cd5aba342
Kamil Lewandowski <klewandowski16@gmail.com>
2024-04-26 22:08:25 +0200

    Add snackbar

---
+++ BudgetBuddy/app/src/main/java/com/kogelmogel123/budgetbuddy/ui/components/SnackBarComponent.kt
@@ -0,0 +1,40 @@
+package com.kogelmogel123.budgetbuddy.ui.components
+
+import android.annotation.SuppressLint
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Image
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import kotlinx.coroutines.launch
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun snackBarComponent(){
+    val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+    Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        },
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                text = { Text("Show snackbar") },
+                icon = { Icon(Icons.Filled.Image, contentDescription = "") },
+                onClick = {
+                    scope.launch {
+                        snackbarHostState.showSnackbar("Snackbar")
+                    }
+                }
+            )
+        }
+    ) {  contentPadding ->
+        // Screen content
+    }
+}
--- BudgetBuddy/app/src/main/java/com/kogelmogel123/budgetbuddy/ui/screens/camera/CameraPreviewScreen.kt
+++ BudgetBuddy/app/src/main/java/com/kogelmogel123/budgetbuddy/ui/screens/camera/CameraPreviewScreen.kt
@@ -12,17 +12,24 @@ import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
@@ -32,35 +39,41 @@ fun CameraPreviewScreen() {
     val lifecycleOwner = LocalLifecycleOwner.current
     val context = LocalContext.current
     val preview = Preview.Builder().build()
-    val previewView = remember {
-        PreviewView(context)
-    }
+    val previewView = remember { PreviewView(context) }
     val cameraxSelector = CameraSelector.Builder().requireLensFacing(lensFacing).build()
-    val imageCapture = remember {
-        ImageCapture.Builder().build()
-    }
+    val imageCapture = remember { ImageCapture.Builder().build() }
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+
     LaunchedEffect(lensFacing) {
         val cameraProvider = context.getCameraProvider()
         cameraProvider.unbindAll()
         cameraProvider.bindToLifecycle(lifecycleOwner, cameraxSelector, preview, imageCapture)
         preview.setSurfaceProvider(previewView.surfaceProvider)
     }
+
     Box(contentAlignment = Alignment.BottomCenter, modifier = Modifier.fillMaxSize()) {
         AndroidView({ previewView }, modifier = Modifier.fillMaxSize())
-        Button(onClick = { captureImage(imageCapture, context) }) {
+        SnackbarHost(hostState = snackbarHostState)
+        Button(onClick = {
+            captureImage(imageCapture, context, snackbarHostState, coroutineScope) },
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 80.dp)
+        ) {
             Text(text = "Capture Image")
         }
     }
 }
 
-@androidx.compose.ui.tooling.preview.Preview
+@androidx.compose.ui.tooling.preview.Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun CameraPreviewScreenPreview() {
     CameraPreviewScreen()
 }
 
-private fun captureImage(imageCapture: ImageCapture, context: Context) {
-    val name = "CameraxImage.jpeg"
+private fun captureImage(imageCapture: ImageCapture, context: Context, snackbarHostState: SnackbarHostState, coroutineScope: CoroutineScope) {
+    val name = "Receipt.jpeg"
     val contentValues = ContentValues().apply {
         put(MediaStore.MediaColumns.DISPLAY_NAME, name)
         put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
@@ -81,10 +94,16 @@ private fun captureImage(imageCapture: ImageCapture, context: Context) {
         object : ImageCapture.OnImageSavedCallback {
             override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
                 println("Successs")
+                coroutineScope.launch {
+                    snackbarHostState.showSnackbar("Image captured successfully")
+                }
             }
 
             override fun onError(exception: ImageCaptureException) {
                 println("Failed $exception")
+                coroutineScope.launch {
+                    snackbarHostState.showSnackbar("Error capturing image")
+                }
             }
 
         })
